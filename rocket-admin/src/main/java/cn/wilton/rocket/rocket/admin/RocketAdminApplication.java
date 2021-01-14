70cdc109c03b2df03ebe81bdb79f69ecf7aedc5a
mapeng <wilton.icp@gmail.com>
2021-01-14 14:30:25 +0800

    基础模块构建

---
+++ rocket-admin/src/main/java/cn/wilton/rocket/rocket/admin/RocketAdminApplication.java
@@ -0,0 +1,13 @@
+package cn.wilton.rocket.rocket.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketAdminApplication.class, args);
+    }
+
+}
